650246ff634f5e90ac2acc19cf268ef8a42a723f
Zhaojian Zhen <zhaojian.zhen@student.manchester.ac.uk>
2020-10-21 12:45:29 +0100

    Spellcheck function now removes all punctuation and numbers and lowers the case of characters. Minor comments added

--- spellchecker.py
+++ spellchecker.py
@@ -1,5 +1,11 @@
+import time, re
 def spellcheck(test):
-	pass
+
+	test = test.lower() #Makes everything lowercase
+	test = re.sub(r"[^\w\s]", " ", test) #Removes punctuation from text	
+	test = re.sub(r"[\b\d+\b]", " ", test) #Removes numbers from the text
+	list = test.split() #Splits the words in the text into items of a list
+	print(list)
 
 def intialmenu():
 	print("\n1. Spellcheck a sentence \n2. Spellcheck a file \n0. Quit program")
@@ -7,27 +13,31 @@ def intialmenu():
 	while True:
 		try:
 			prompt = int(input("\nPlease select an option by entering its corresponding number: "))
+			#Input validation
 			if prompt in set([0, 1, 2]):
 				break
 			else:
 				print("Please try again.")
 		except ValueError:
-			print("Please try again.")
+			print("Please try again.") 
 	optionselect(prompt)
 
 def optionselect(option):
 
 	if option == 1:
-		sentence = input("Please enter your sentence: ")
+		sentence = input("\nPlease enter your sentence: ")
 		spellcheck(sentence)
 	elif option == 2:
 		while True:
 			try:
-				filename = input("Please enter the filename: ")
-				file = open(filename, "r")
+				filename = input("\nPlease enter the filename: ")
+				f = open(filename, "r")
+				file=f.read()
 				break
+				#More input validation
 			except FileNotFoundError:
 				print("\nCannot find the file with filename " + filename + ".")
+				#Another menu to either try again or return to start menu
 				while True:
 					try:
 						option = int(input("\nEnter\n1. To try another filename\n2. To return to initial menu\n: "))
