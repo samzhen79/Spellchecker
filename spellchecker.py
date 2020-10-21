fe9c58e479a91abccc29cc4ae6223fc597f14124
Zhaojian Zhen <zhaojian.zhen@student.manchester.ac.uk>
2020-10-21 11:51:28 +0100

    Program now prompts for sentence or filename after option is chosen. Program split up into functions for better flow. Improved formatting

--- spellchecker.py
+++ spellchecker.py
@@ -1,13 +1,42 @@
-print("1. Spellcheck a sentence \n2. Spellcheck a file \n0. Quit program")
-option = 10
-while True:
-	try:
-		option = int(input("Please select an option by entering its corresponding number: "))
-		if option in set([0, 1, 2]):
-			break
-		else:
-			print("Please try again.")
-	except ValueError:
-		print("Please try again")
+def spellcheck(test):
+	pass
 
-	
+def intialmenu():
+	print("\n1. Spellcheck a sentence \n2. Spellcheck a file \n0. Quit program")
+
+	while True:
+		try:
+			prompt = int(input("\nPlease select an option by entering its corresponding number: "))
+			if prompt in set([0, 1, 2]):
+				break
+			else:
+				print("Please try again.")
+		except ValueError:
+			print("Please try again.")
+	optionselect(prompt)
+
+def optionselect(option):
+
+	if option == 1:
+		sentence = input("Please enter your sentence: ")
+		spellcheck(sentence)
+	elif option == 2:
+		while True:
+			try:
+				filename = input("Please enter the filename: ")
+				file = open(filename, "r")
+				break
+			except FileNotFoundError:
+				print("\nCannot find the file with filename " + filename + ".")
+				while True:
+					try:
+						option = int(input("\nEnter\n1. To try another filename\n2. To return to initial menu\n: "))
+						if option == 1:
+							break
+						elif option == 2:
+							intialmenu()
+					except ValueError:
+						print("Please try again")
+		spellcheck(file)
+
+intialmenu()
