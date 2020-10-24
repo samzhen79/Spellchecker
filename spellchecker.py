a1f2722b5371dd972fd1f9783a4b226e098e3c7c
Zhaojian Zhen <zhaojian.zhen@student.manchester.ac.uk>
2020-10-24 03:11:52 +0100

    Moved spellcheck function to bottom so it correctly represents the order it shall be used in. Formatted the long strings and added more whitespace for readability.

--- spellchecker.py
+++ spellchecker.py
@@ -1,4 +1,79 @@
 import time, re
+
+def intialmenu(): #Starting menu to allow user to choose how they want to use the program
+
+	print("\n 1. Spellcheck a sentence"\
+		"\n 2. Spellcheck a file" \
+		"\n 0. Quit program")
+
+	while True:
+		try:
+			prompt = int(input("\nPlease select an option by entering its corresponding number: "))
+			#Input validation
+			if prompt in set([0, 1, 2]):
+
+				break
+
+			else:
+
+				print("\nPlease try again.")
+				time.sleep(0.5)
+
+		except ValueError:
+
+			print("\nPlease try again.")
+			time.sleep(0.5)
+
+	optionselect(prompt)
+
+def optionselect(option): #Prompts the user for either the sentence or file depending on the option chosen
+
+	if option == 1:
+
+		sentence = input("\nPlease enter your sentence: ")
+		spellcheck(sentence)
+
+	elif option == 2:
+
+		while True:
+
+			try:
+
+				filename = input("\nPlease enter the filename: ")
+				f = open(filename, "r") #Open the given file to read
+				file=f.read()
+				f.close() #Close the file as it is now not in use
+				break
+				#More input validation
+			except FileNotFoundError:
+
+				print("\nCannot find the file with filename " + filename + ".")
+				time.sleep(0.5)
+				#Another menu to either try again or return to start menu
+				while True:
+
+					try:
+
+						option = int(input("\nEnter"\
+							"\n1. To try another filename"\
+							"\n2. To return to initial menu"\
+							"\n: "))
+
+						if option == 1:
+
+							break
+
+						elif option == 2:
+
+							intialmenu()
+
+					except ValueError:
+
+						print("\nPlease try again")
+						time.sleep(0.5)
+
+		spellcheck(file)
+
 def spellcheck(checkstring):
 
 	checkstring = checkstring.lower() #Makes everything lowercase
@@ -11,69 +86,26 @@ def spellcheck(checkstring):
 	wordslist = words.splitlines() #Splits the words in the file by line into items of a list
 
 	#Assigning variables for statistics
-	totalwordcount = 0
-	correctwordcount = 0
-	incorrectwordcount = 0
+	totalwordcount, correctwordcount, incorrectwordcount = 0, 0, 0
 
 	for x in checklist: #Loops through each word of the list that we are spellchecking
 
 		if (x in wordslist) == False: #Checks if the word is in the EnglishWords.txt
+
 			print(x + " is spelt incorrectly")
 			incorrectwordcount = incorrectwordcount + 1
+
 		else: 
+
 			print(x)
 			correctwordcount = correctwordcount + 1
 
 		totalwordcount = totalwordcount + 1 #Counter
 
-	print("\nSummary:\nTotal number of words: " + str(totalwordcount) + "\nCorrectly spelt words: " + str(correctwordcount) + "\nIncorrectly spelt words: " + str(incorrectwordcount)) #Wordcount
+	print("\nSummary:"\
+		"\nTotal number of words: " + str(totalwordcount) + \
+		"\nCorrectly spelt words: " + str(correctwordcount) + \
+		"\nIncorrectly spelt words: " + str(incorrectwordcount)) #Wordcount
 
-
-def intialmenu(): #Starting menu to allow user to choose how they want to use the program
-	print("\n1. Spellcheck a sentence \n2. Spellcheck a file \n0. Quit program")
-
-	while True:
-		try:
-			prompt = int(input("\nPlease select an option by entering its corresponding number: "))
-			#Input validation
-			if prompt in set([0, 1, 2]):
-				break
-			else:
-				print("\nPlease try again.")
-				time.sleep(0.5)
-		except ValueError:
-			print("\nPlease try again.")
-			time.sleep(0.5)
-	optionselect(prompt)
-
-def optionselect(option): #Prompts the user for either the sentence or file depending on the option chosen
-
-	if option == 1:
-		sentence = input("\nPlease enter your sentence: ")
-		spellcheck(sentence)
-	elif option == 2:
-		while True:
-			try:
-				filename = input("\nPlease enter the filename: ")
-				f = open(filename, "r") #Open the given file to read
-				file=f.read()
-				f.close() #Close the file as it is now not in use
-				break
-				#More input validation
-			except FileNotFoundError:
-				print("\nCannot find the file with filename " + filename + ".")
-				time.sleep(0.5)
-				#Another menu to either try again or return to start menu
-				while True:
-					try:
-						option = int(input("\nEnter\n1. To try another filename\n2. To return to initial menu\n: "))
-						if option == 1:
-							break
-						elif option == 2:
-							intialmenu()
-					except ValueError:
-						print("\nPlease try again")
-						time.sleep(0.5)
-		spellcheck(file)
 
 intialmenu() #Starts the program
